b461d6cbf817e671cad3d3e65e06b05ef196b515
Steve Gossip <stephengossip@gmail.com>
2021-05-26 19:42:04 +0100

    added basic authentication for the sheety API.

--- main.py
+++ main.py
@@ -5,13 +5,8 @@ import datetime
 APP_ID = os.environ.get("NUTRITIONIX_APP_ID")
 API_KEY = os.environ.get("NUTRITIONIX_API_KEY")
 SHEETY_USERNAME = os.environ.get("SHEETY_USERNAME")
-current_date = datetime.datetime.now()
-date_format = "%d/%m/%Y"
-time_format = "%H:%M:%S"
 
 nutritionix_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
-sheety_endpoint = f"https://api.sheety.co/{SHEETY_USERNAME}/workoutTracking/workouts"
-
 
 headers = {
     "x-app-key": API_KEY,
@@ -27,17 +22,22 @@ response = requests.post(url=nutritionix_endpoint, json=params, headers=headers)
 data = response.json()
 daily_exercises = data['exercises']
 
+sheety_endpoint = f"https://api.sheety.co/{SHEETY_USERNAME}/workoutTracking/workouts"
+auth_header = {
+    "Authorization": os.environ.get("AUTH_HEADER")
+}
+current_date = datetime.datetime.now()
 
 for exercise in daily_exercises:
     payload = {
         "workout": {
-            "date": current_date.strftime(date_format),
-            "time": current_date.strftime(time_format),
-            "exercise": exercise['name'],
+            "date": current_date.strftime("%d/%m/%Y"),
+            "time": current_date.strftime("%X"),
+            "exercise": exercise['name'].title(),
             "duration": exercise['duration_min'],
             "calories": exercise['nf_calories']
         }
     }
     print(payload)
-    sheety_response = requests.post(url=sheety_endpoint, json=payload)
+    sheety_response = requests.post(url=sheety_endpoint, json=payload, headers=auth_header)
     print(sheety_response.text)
